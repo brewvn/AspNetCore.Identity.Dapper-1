850290f6ead6f701340a295bacb9f5dbdf7f9c7d
Lewis <zlzforever@163.com>
2018-04-24 23:40:50 +0800

    start identity dapper

---
+++ src/AspNetCore.Identity.Dapper/ServiceCollectionExtensions.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Data;
+using System.Reflection;
+
+namespace AspNetCore.Identity.Dapper
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IdentityBuilder AddDapperStores<TDbConnection>(this IdentityBuilder builder) where TDbConnection : IDbConnection
+		{
+			AddStores(builder.Services, builder.UserType, builder.RoleType, typeof(TDbConnection));
+			return builder;
+		}
+
+		private static void AddStores(IServiceCollection services, Type userType, Type roleType, Type connection)
+		{
+			var identityUserType = FindGenericBaseType(userType, typeof(IdentityUser<>));
+			if (identityUserType == null)
+			{
+				throw new InvalidOperationException($"{nameof(userType)} is not a IdentityUser.");
+			}
+
+			var keyType = identityUserType.GenericTypeArguments[0];
+
+			if (roleType != null)
+			{
+				var identityRoleType = FindGenericBaseType(roleType, typeof(IdentityRole<>));
+				if (identityRoleType == null)
+				{
+					throw new InvalidOperationException($"{nameof(userType)} is not a IdentityRole.");
+				}
+
+				Type userStoreType = null;
+				Type roleStoreType = null;
+				var connectionType = FindGenericBaseType(connection, typeof(IDbConnection));
+				if (connectionType == null)
+				{
+					userStoreType = typeof(UserStore<>).MakeGenericType(userType);
+					//roleStoreType = typeof(RoleStore<,,>).MakeGenericType(roleType, connection, keyType);
+				}
+				else
+				{
+					//userStoreType = typeof(UserStore<,,,,,,,,>).MakeGenericType(userType, roleType, connection,
+					//	connectionType.GenericTypeArguments[2],
+					//	connectionType.GenericTypeArguments[3],
+					//	connectionType.GenericTypeArguments[4],
+					//	connectionType.GenericTypeArguments[5],
+					//	connectionType.GenericTypeArguments[7],
+					//	connectionType.GenericTypeArguments[6]);
+					//roleStoreType = typeof(RoleStore<,,,,>).MakeGenericType(roleType, connection,
+					//	connectionType.GenericTypeArguments[2],
+					//	connectionType.GenericTypeArguments[4],
+					//	connectionType.GenericTypeArguments[6]);
+				}
+				services.TryAddScoped(typeof(IUserStore<>).MakeGenericType(userType), userStoreType);
+				services.TryAddScoped(typeof(IRoleStore<>).MakeGenericType(roleType), roleStoreType);
+			}
+			else
+			{   // No Roles
+				//	Type userStoreType = null;
+				//	var connectionType = FindGenericBaseType(connection, typeof(connection));
+				//	if (connectionType == null)
+				//	{
+				//		// If its a custom DbContext, we can only add the default POCOs
+				//		userStoreType = typeof(UserOnlyStore<,,>).MakeGenericType(userType, connection, keyType);
+				//	}
+				//	else
+				//	{
+				//		userStoreType = typeof(UserOnlyStore<,,,,,>).MakeGenericType(userType, connection,
+				//			connectionType.GenericTypeArguments[1],
+				//			connectionType.GenericTypeArguments[2],
+				//			connectionType.GenericTypeArguments[3],
+				//			connectionType.GenericTypeArguments[4]);
+				//	}
+				//services.TryAddScoped(typeof(IUserStore<>).MakeGenericType(userType), userStoreType);
+			}
+		}
+
+		private static TypeInfo FindGenericBaseType(Type currentType, Type genericBaseType)
+		{
+			TypeInfo typeInfo = currentType.GetTypeInfo();
+			while (typeInfo.BaseType != (Type)null)
+			{
+				typeInfo = typeInfo.BaseType.GetTypeInfo();
+				Type left = typeInfo.IsGenericType ? typeInfo.GetGenericTypeDefinition() : null;
+				if (left != (Type)null && left == genericBaseType)
+				{
+					return typeInfo;
+				}
+			}
+			return null;
+		}
+	}
+}
---
+++ src/AspNetCore.Identity.Dapper/UserStore.cs
@@ -0,0 +1,283 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace AspNetCore.Identity.Dapper
+{
+	public interface ISqlGenerator
+	{
+		string TableName { get; }
+
+		string PasswordHash { get; }
+
+		string EmailConfirmed { get; }
+
+		string NormalizedEmail { get; }
+
+		string Email { get; }
+
+		string NormalizedUserName { get; }
+
+		string UserName { get; }
+	}
+
+	public class SqlGenerator : ISqlGenerator
+	{
+		public virtual string TableName => "TableName";
+
+		public virtual string PasswordHash => "PasswordHash";
+
+		public virtual string EmailConfirmed => "EmailConfirmed";
+
+		public virtual string NormalizedEmail => "NormalizedEmail";
+
+		public virtual string Email => "Email";
+
+		public virtual string NormalizedUserName => "NormalizedUserName";
+
+		public virtual string UserName => "UserName";
+
+		public virtual string CreateUserSql => "";
+
+		public virtual string DeleteUserSql => $"DELETE FROM {TableName} WHERE Id = @Id;";
+
+		public virtual string FindByEmail => $"SELECT * FROM {TableName} Where {Email} = @Email;";
+
+		public virtual string FindById => $"SELECT * FROM {TableName} Where Id = @Id;";
+
+		public virtual string SetEmail => $"UPDATE {TableName} SET {Email} = @Email WHERE Id = @Id;";
+
+		public virtual string SetEmailConfirmed => $"UPDATE {TableName} SET {EmailConfirmed} = @EmailConfirmed WHERE Id = @Id;";
+
+		public virtual string SetNormalizedEmail => $"UPDATE {TableName} SET {NormalizedEmail} = @NormalizedEmail WHERE Id = @Id;";
+
+		public virtual string SetNormalizedUserName => $"UPDATE {TableName} SET {NormalizedUserName} = @NormalizedUserName WHERE Id = @Id;";
+
+		public virtual string SetPasswordHash => $"UPDATE {TableName} SET {PasswordHash} = @PasswordHash WHERE Id = @Id;";
+
+		public virtual string SetUserName => $"UPDATE {TableName} SET {UserName} = @UserName WHERE Id = @Id;";
+
+		public virtual string Update => $"UPDATE {TableName} SET {UserName} = @UserName WHERE Id = @Id;";
+	}
+
+	/// <summary>
+	/// This store is only partially implemented. It supports user creation and find methods.
+	/// </summary>
+	public class UserStore<TUser> : IUserStore<TUser>,
+		IUserPasswordStore<TUser>,
+		IUserEmailStore<TUser>
+		where TUser : IdentityUser
+	{
+		private readonly SqlGenerator _sqlGenerator;
+		private readonly IDbConnection _connection;
+
+		public UserStore(SqlGenerator sqlGenerator, IDbConnection connection)
+		{
+			_sqlGenerator = sqlGenerator;
+			_connection = connection;
+		}
+
+		#region createuser
+		public async Task<IdentityResult> CreateAsync(TUser user,
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+			var rows = await _connection.ExecuteAsync(_sqlGenerator.CreateUserSql, user);
+			if (rows > 0)
+			{
+				return IdentityResult.Success;
+			}
+			else
+			{
+				return IdentityResult.Failed(new IdentityError { Description = $"Could not insert user {user}." });
+			}
+		}
+		#endregion
+
+		public async Task<IdentityResult> DeleteAsync(TUser user,
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			int rows = await _connection.ExecuteAsync(_sqlGenerator.DeleteUserSql, new { user.Id });
+
+			if (rows > 0)
+			{
+				return IdentityResult.Success;
+			}
+			return IdentityResult.Failed(new IdentityError { Description = $"Could not delete user {user.Email}." });
+		}
+
+		public void Dispose()
+		{
+		}
+
+		public async Task<TUser> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken)
+		{
+			return await _connection.QueryFirstAsync<TUser>(_sqlGenerator.CreateUserSql, new { NormalizedEmail = normalizedEmail });
+		}
+
+		public async Task<TUser> FindByIdAsync(string userId,
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (userId == null) throw new ArgumentNullException(nameof(userId));
+			Guid idGuid;
+			if (!Guid.TryParse(userId, out idGuid))
+			{
+				throw new ArgumentException("Not a valid Guid id", nameof(userId));
+			}
+
+			return await _connection.QueryFirstAsync<TUser>(_sqlGenerator.CreateUserSql, new { Id = userId });
+		}
+
+		public async Task<TUser> FindByNameAsync(string userName,
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (userName == null) throw new ArgumentNullException(nameof(userName));
+
+			return await _connection.QueryFirstAsync<TUser>(_sqlGenerator.CreateUserSql, new { UserName = userName });
+		}
+
+		public Task<string> GetEmailAsync(TUser user, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			return Task.FromResult(user.Email);
+		}
+
+		public Task<bool> GetEmailConfirmedAsync(TUser user, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			return Task.FromResult(user.EmailConfirmed);
+		}
+
+		public Task<string> GetNormalizedEmailAsync(TUser user, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			return Task.FromResult(user.NormalizedEmail);
+		}
+
+		public Task<string> GetNormalizedUserNameAsync(TUser user, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			return Task.FromResult(user.NormalizedUserName);
+		}
+
+		public Task<string> GetPasswordHashAsync(TUser user, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			return Task.FromResult(user.PasswordHash);
+		}
+
+		public Task<string> GetUserIdAsync(TUser user, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			return Task.FromResult(user.Id.ToString());
+		}
+
+		public Task<string> GetUserNameAsync(TUser user, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			return Task.FromResult(user.UserName);
+		}
+
+		public async Task<bool> HasPasswordAsync(TUser user, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			return await Task.FromResult(!string.IsNullOrWhiteSpace(user.PasswordHash));
+		}
+
+		public async Task SetEmailAsync(TUser user, string email, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+			if (string.IsNullOrWhiteSpace(email)) throw new ArgumentNullException(nameof(email));
+
+			await _connection.ExecuteAsync(_sqlGenerator.CreateUserSql, new { user.Id, Email = email });
+		}
+
+		public async Task SetEmailConfirmedAsync(TUser user, bool confirmed, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			await _connection.ExecuteAsync(_sqlGenerator.CreateUserSql, new { user.Id, EmailConfirmed = confirmed });
+		}
+
+		public async Task SetNormalizedEmailAsync(TUser user, string normalizedEmail, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+			if (string.IsNullOrWhiteSpace(normalizedEmail)) throw new ArgumentNullException(nameof(normalizedEmail));
+
+			await _connection.ExecuteAsync(_sqlGenerator.CreateUserSql, new { user.Id, NormalizedEmail = normalizedEmail });
+		}
+
+		public async Task SetNormalizedUserNameAsync(TUser user, string normalizedName, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+			if (string.IsNullOrWhiteSpace(normalizedName)) throw new ArgumentNullException(nameof(normalizedName));
+
+			await _connection.ExecuteAsync(_sqlGenerator.CreateUserSql, new { user.Id, NormalizedName = normalizedName });
+		}
+
+		public async Task SetPasswordHashAsync(TUser user, string passwordHash, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+			if (string.IsNullOrWhiteSpace(passwordHash)) throw new ArgumentNullException(nameof(passwordHash));
+
+			await _connection.ExecuteAsync(_sqlGenerator.CreateUserSql, new { user.Id, PasswordHash = passwordHash });
+		}
+
+		public async Task SetUserNameAsync(TUser user, string userName, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+			if (string.IsNullOrWhiteSpace(userName)) throw new ArgumentNullException(nameof(userName));
+
+			await _connection.ExecuteAsync(_sqlGenerator.CreateUserSql, new { user.Id, UserName = userName });
+		}
+
+		public async Task<IdentityResult> UpdateAsync(TUser user, CancellationToken cancellationToken)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (user == null) throw new ArgumentNullException(nameof(user));
+
+			var rows = await _connection.ExecuteAsync(_sqlGenerator.Update, user);
+			if (rows > 0)
+			{
+				return IdentityResult.Success;
+			}
+			else
+			{
+				return IdentityResult.Failed(new IdentityError { Description = $"Update user {user} failed." });
+			}
+		}
+	}
+}
